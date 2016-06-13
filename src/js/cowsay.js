965293e32624446920e5637a60fc17733a4303c5
David <goodwid@gmail.com>
2016-06-13 14:19:26 -0700

    Hope this works!

---
+++ src/js/cowsay.js
@@ -0,0 +1,3 @@
+const cowsay = require('cowsay');
+
+module.exports = moo => cowsay(moo);
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const hwp = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: './dist',
+    filename: 'bundle.js'
+  },
+  devtool: 'source-map',
+  plugins: [new hwp({
+    template: './src/index.html'
+  })],
+  module: {
+    preLoaders: [{
+      test: /\.js$/,
+      loader: 'eslint-loader',
+      exclude: /node_modules/
+    }],
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['es-2015']
+      }
+    },
+    {
+      test:/\.css$/,
+      loader: 'style!css'
+    }]
+  }
+};
