e5dc09fd9bf0ba1549e26e3f944f81e14541ab8e
Yvonne Hayes <queyvonne@gmail.com>
2016-06-13 15:37:50 -0700

    removed greeting added text

--- app/src/app.js
+++ app/src/app.js
@@ -4,10 +4,6 @@ const cowsay = require('./cowsay');
 require('./css/main.css');
 
 const app = angular.module('app', [])
-.controller('main', function($scope) {
-  $scope.place = 'pdx';
-  $scope.greeting = greeting;
-});
-app.controller('cow', function($scope) {
+.controller('cow', function($scope) {
   $scope.cowsay = cowsay;
 });
