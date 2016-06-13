298b4269028a2ecf2869959ad19076c4730f86df
Yvonne Hayes <queyvonne@gmail.com>
2016-06-13 14:10:41 -0700

    added webpack.config.js

---
+++ app/webpack.config.js
@@ -0,0 +1,32 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: '../server/public',
+    filename: 'bundle.js'
+  },
+  devtool: 'source-map',
+  plugins: [new HtmlWebpackPlugin({
+    template: './src/index.html'
+  })],
+  module: {
+    preLoaders: [{
+      test:/\.js$/,
+      loader: 'eslint-loader',
+      exclude: /node_modules/
+    }],
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['es2015']
+      }
+    },
+    {
+      test: /\.css$/,
+      loader:'style!css'
+    }]
+  }
+};
