1f316190b0f1ff6ecd8d67f6bfbde3e42dec8477
Yvonne Hayes <queyvonne@gmail.com>
2016-06-14 08:40:36 -0700

    two cows now talk to each other

--- app/src/app.js
+++ app/src/app.js
@@ -1,6 +1,7 @@
 const angular = require ('angular');
 const greeting = require('./greeting');
 const cowsay = require('./cowsay');
+const cowsay2 = require('./cowsay2');
 require('./css/main.css');
 
 const app = angular.module('app', [])
@@ -8,6 +9,11 @@ const app = angular.module('app', [])
   $scope.cowsay = cowsay;
 });
 
+
 app.controller('cow2', function($scope) {
-  $scope.cowsay = cowsay;
+  $scope.cowsay2 = cowsay2;
+});
+
+app.controller('greetz', function($scope){
+  $scope.hello = 'Make the cows talk!';
 });
--- app/src/cowsay.js
+++ app/src/cowsay.js
@@ -1,14 +1,8 @@
 var cowsay = require('cowsay-browser');
 
-module.exports = function(moo = 'hi there') {
+module.exports = function(moo = 'Sup, bro?') {
   return cowsay.say({
     text : `${moo}`,
     T: 'U'
-  });
-};
-
-module.exports = function(moomoo) {
-  return cowsay.say({
-    text: 'Hello there kind sir'
   });
 };
